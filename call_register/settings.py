e9c7b6f58f19a0b3af424b877ccccf1190d9f19b
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-06-17 09:44:56 -0300

    fix allowed host

--- call_register/settings.py
+++ call_register/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://api-registercall.herokuapp.com']
+ALLOWED_HOSTS = ['api-registercall.herokuapp.com', 'localhost']
 
 
 # Application definition
